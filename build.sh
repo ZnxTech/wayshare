e43051efa205017e411e2c92771689e373720c66
ZnxTech <levidaniel2610@gmail.com>
2024-10-03 10:47:10 +0300

    better protocol organization & added cosmic protocols

--- build.sh
+++ build.sh
@@ -2,24 +2,31 @@
 
 # wayshare build script
 
+# printf colors
 NORM='\033[0m'
 HIGH='\033[1;32m'
 
 printf "| --- ${HIGH}creating protocol c files${NORM} --- |\n"
 
-# create wlroots screencopy header from xml protocol
-wayland-scanner private-code \
-    ./wl_protocols/wlr-screencopy-unstable-v1.xml \
-    ./src/wlr-screencopy-unstable-v1.c
+# create screencopy c/hh files from xml protocols
+mkdir lib_protocols -p
 
-wayland-scanner client-header \
-    ./wl_protocols/wlr-screencopy-unstable-v1.xml \
-    ./src/wlr-screencopy-unstable-v1.hh
+for file in ./wl_protocols/*; do
+    filename="$(basename $file .xml)"
+    wayland-scanner private-code \
+        ./wl_protocols/$filename.xml \
+        ./lib_protocols/$filename.c
+
+    wayland-scanner client-header \
+        ./wl_protocols/$filename.xml \
+        ./lib_protocols/$filename.hh
+done
 
 printf "| --- ${HIGH}compiling debug${NORM} --- |\n"
 
 # compile debug version
-gcc ./src/* \
+gcc ./src/* ./lib_protocols/* \
+    -I ./lib_protocols \
     -o ./bin/wayland \
     -x c++ \
     -shared-libgcc \
@@ -31,7 +38,8 @@ gcc ./src/* \
 printf "| --- ${HIGH}compiling release${NORM} --- |\n"
 
 # compile release version
-gcc ./src/* \
+gcc ./src/* ./lib_protocols/* \
+    -I ./lib_protocols \
     -o ./bin/wayland_r \
     -x c++ \
     -shared-libgcc \
