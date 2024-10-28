2599e9575ed5a2d61ef7fea463551e42dc9b82df
ZnxTech <levidaniel2610@gmail.com>
2024-10-28 19:41:28 +0200

    fix main for state & wl_connect changes

--- src/main.cc
+++ src/main.cc
@@ -1,21 +1,16 @@
 #include <wayland-client.h>
 #include "logger.hh"
 #include "wayland.hh"
+#include "shm.hh"
 
 int main(int argc, char *argv[]) {
-
     // connection init
-    struct wl_state state = {};
-    if (!wl_connect(&state, NULL)) {
-        return 1;
-    }
-
-    // dispatch loop
-    while (wl_display_dispatch(state.display) != -1) {
-
+    struct wl_state *state = wl_connect(NULL);
+    if (state == NULL) {
+        return -1;
     }
 
     // fin
-    wl_disconnect(&state);
+    wl_disconnect(state);
     return 0;
 }
